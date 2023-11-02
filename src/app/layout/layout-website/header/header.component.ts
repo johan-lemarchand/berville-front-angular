99732ad4757cd5fc780561fc722dcbc4f094cea0
Johan Lemarchand <johan27000@gmail.com>
2023-11-02 18:59:30 +0100

    first commit

---
+++ src/app/layout/layout-website/header/header.component.ts
@@ -0,0 +1,81 @@
+import {Component, OnDestroy, OnInit, ViewEncapsulation} from '@angular/core';
+import {MatButtonModule} from "@angular/material/button";
+import {NgIf} from "@angular/common";
+import {MatIconModule} from "@angular/material/icon";
+import {Subject, takeUntil} from "rxjs";
+import {FuseMediaWatcherService} from "../../../../@fuse/services/media-watcher";
+import {FuseNavigationService, FuseVerticalNavigationComponent} from "../../../../@fuse/components/navigation";
+import { Navigation } from 'app/core/navigation/navigation.types';
+import {NavigationService} from "../../../core/navigation/navigation.service";
+import {RouterLink, RouterOutlet} from "@angular/router";
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    standalone: true,
+    imports: [
+        MatButtonModule,
+        NgIf,
+        MatIconModule,
+        FuseVerticalNavigationComponent,
+        RouterLink,
+        RouterOutlet,
+    ]
+})
+
+export class HeaderComponent implements OnInit, OnDestroy {
+    isScreenSmall: boolean;
+    navigation: Navigation;
+    private _unsubscribeAll: Subject<any> = new Subject<any>();
+
+    constructor(
+        private _fuseMediaWatcherService: FuseMediaWatcherService,
+        private _fuseNavigationService: FuseNavigationService,
+        private _navigationService: NavigationService,
+    ) {
+    }
+
+     ngOnInit() {
+        // Subscribe to navigation data
+        this._navigationService.navigation$
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe((navigation: Navigation) =>
+            {
+                this.navigation = navigation;
+            });
+
+        this._fuseMediaWatcherService.onMediaChange$
+                .pipe(takeUntil(this._unsubscribeAll))
+                .subscribe(({matchingAliases}) =>
+                {
+                    // Check if the screen is small
+                    this.isScreenSmall = !matchingAliases.includes('md');
+            });
+  }
+
+  ngOnDestroy(): void
+    {
+        // Unsubscribe from all subscriptions
+        this._unsubscribeAll.next(null);
+        this._unsubscribeAll.complete();
+    }
+
+  /**
+     * Toggle navigation
+     *
+     * @param name
+     */
+    toggleNavigation(name: string): void
+    {
+        // Get the navigation
+        const navigation = this._fuseNavigationService.getComponent<FuseVerticalNavigationComponent>(name);
+
+        if ( navigation )
+        {
+            // Toggle the opened status
+            navigation.toggle();
+        }
+    }
+}
---
+++ src/app/mock-api/common/navigation/data.ts
@@ -0,0 +1,72 @@
+/* eslint-disable */
+import { FuseNavigationItem } from '@fuse/components/navigation';
+
+export const defaultNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    }
+];
+export const compactNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    }
+];
+export const futuristicNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    }
+];
+export const horizontalNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    }
+];
+
+export const websiteNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'home',
+        title: 'Home',
+        type : 'basic',
+        link : '/'
+    },
+    {
+        id   : 'club',
+        title: 'Club',
+        type : 'basic',
+        link : '/club'
+    },
+    {
+        id   : 'actualites',
+        title: 'Actualités',
+        type : 'basic',
+        link : '/actualites'
+    },
+    {
+        id   : 'event',
+        title: 'Event',
+        type : 'basic',
+        link : '/event'
+    },
+    {
+        id   : 'contact',
+        title: 'Contact',
+        type : 'basic',
+        link : '/contact'
+    },
+];
